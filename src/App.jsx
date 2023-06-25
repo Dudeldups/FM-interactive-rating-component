5ec0cd56029f7134b7373899d48c797faf214218
BananenArni <130840918+Dudeldups@users.noreply.github.com>
2023-06-25 22:02:20 +0200

    added basic rating card styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
+
+import Rating from "./components/Rating";
+import Confirmation from "./components/Confirmation";
+
+import "./scss/main.scss";
+
 export default function App() {
+  const [isSubmitted, setisSubmitted] = useState(false);
+  const [rating, setRating] = useState("");
+
+  function handleChange(e) {
+    setRating(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (rating !== "") {
+      setisSubmitted(true);
+    }
+  }
+
   return (
     <>
       <header>
         <h1 className="sr-only">This is a Frontend Mentor challenge</h1>
       </header>
-      <main></main>
+      <main className="card">
+        {isSubmitted ? (
+          <Confirmation rating={rating} />
+        ) : (
+          <Rating
+            rating={rating}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+          />
+        )}
+      </main>
     </>
   );
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,3 +1,45 @@
-export default function Rating() {
-  return <div>Rating</div>;
+import "./Rating.scss";
+
+export default function Rating({ rating, handleChange, handleSubmit }) {
+  const possibleRatings = ["1", "2", "3", "4", "5"];
+
+  const radioInputs = possibleRatings.map(num => (
+    <>
+      <label htmlFor={num}>
+        <input
+          key={num}
+          id={num}
+          type="radio"
+          value={num}
+          name="ratings"
+          checked={rating === num}
+          onChange={handleChange}
+        />
+        {num}
+      </label>
+    </>
+  ));
+
+  return (
+    <div className="form">
+      <div className="form__img-wrapper">
+        <img src="./src/assets/icon-star.svg" alt="" />
+      </div>
+      <form>
+        <fieldset>
+          <legend className="form__text">
+            <h2 className="form__text__title">How did we do?</h2>
+            <p className="form__text__description">
+              Please let us know how we did with your support request. All
+              feedback is appreciated to help us improve our offering!
+            </p>
+          </legend>
+          <div className="form__input-container">{radioInputs}</div>
+          <button className="form__button" onClick={handleSubmit}>
+            Submit
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
